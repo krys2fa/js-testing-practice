0d04b2499d96f5c8369fcc2b1bb71af7728bf664
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-09-01 14:25:48 +0300

    write tests for the caesar cipher function

---
+++ tests/caesar.test.js
@@ -0,0 +1,13 @@
+const caesarCipher = require('../src/caesar')
+
+test('lowercase string codes with caesar cipher', () => {
+  expect(caesarCipher('abc', 1)).toBe('bcd');
+})
+
+test('uppercase string codes with caesar cipher', () => {
+  expect(caesarCipher('I am learning JS', 5)).toBe('N fr qjfwsnsl OX');
+})
+
+test('wrapping string codes with caesar cipher', () => {
+  expect(caesarCipher('xyz', 5)).toBe('cde');
+})
