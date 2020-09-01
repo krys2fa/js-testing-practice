67f17534984ffdd2193edc918a2b25b1f7623d47
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-09-01 17:51:38 +0300

    add more tests for capitalize and reverse string functions

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -3,3 +3,7 @@ const capitalize = require('../src/capitalize');
 test('capitalizes the first character in the string', () => {
   expect(capitalize('hello')).toBe('Hello');
 });
+
+test('capitalizes the first character in the string', () => {
+  expect(capitalize('coding is cool')).toBe('Coding is cool');
+});
--- tests/reverse-string.test.js
+++ tests/reverse-string.test.js
@@ -3,3 +3,11 @@ const reverseString = require('../src/reverse-string');
 test('reverses a string', () => {
   expect(reverseString('hello')).toBe('olleh');
 });
+
+test('reverses a string(Javascript)', () => {
+  expect(reverseString('Javascript')).toBe('tpircsavaJ');
+});
+
+test('gives the reverse of string(Microverse)', () => {
+  expect(reverseString('Microverse')).toBe('esrevorciM');
+});
